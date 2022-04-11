604c5e88dd57cf49eb9f266eb2e238359aefda3b
Damla Koksal <me@damlakoksal.com>
2022-04-11 13:51:22 +0300

    feat: scaffold post

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
@@ -6,13 +5,9 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <h1>AboutPage</h1>
       <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
       </p>
     </>
   )
